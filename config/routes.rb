47e90b5a0d1b8a1c80fd578a42145852837bf931
angelali92 <angela@li>
2014-08-08 15:15:07 -0700

    test merge conflict 2

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ MMApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  get 'users' => 'user#index'
+  get 'hello' => 'user#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
