c2a1f7412e1a85fa6ad377de8dfb18f1ff3ec0cb
angelali92 <angela@li>
2014-08-08 15:01:03 -0700

    test

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ MMApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  get 'products' => 'products#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
